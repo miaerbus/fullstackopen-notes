b0ec3a37a8677524826919cc66c4373938a7f797
Mia Erbus <mia.erbus@gmail.com>
2020-04-18 21:05:35 +0200

    [part4] user administration, token authentication

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,10 @@
 const notesRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Note = require('../models/note')
+const User = require('../models/user')
 
 notesRouter.get('/', async (request, response) => {
-  const notes = await Note.find({})
+  const notes = await Note.find({}).populate('user', { username: 1, name: 1 })
   response.json(notes.map((note) => note.toJSON()))
 })
 
@@ -23,6 +25,14 @@ notesRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
+const getTokenFrom = (request) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
+
 notesRouter.post('/', async (request, response) => {
   const body = request.body
 
@@ -30,13 +40,24 @@ notesRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: 'content missing' })
   }
 
+  const token = getTokenFrom(request)
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+  const user = await User.findById(decodedToken.id)
+
   const note = new Note({
     content: body.content,
     important: body.important === undefined ? false : body.important,
     date: new Date(),
+    user: user._id,
   })
 
   const savedNote = await note.save()
+  user.notes = user.notes.concat(savedNote._id)
+  await user.save()
+
   response.json(savedNote.toJSON())
 })
 
